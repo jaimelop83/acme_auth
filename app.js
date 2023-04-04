1c509d4083eabd68435b4fa5a3499893c1e385b7
jaimelop83 <jaime.a.lopez@gmail.com>
2023-04-03 22:58:16 -0400

    Postman and backend working - front end does not work.

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const app = express();
 const { models: { User }} = require('./db');
 const path = require('path');
 
+require('dotenv').config();
+
 // middleware
 app.use(express.json());
+
+// Headers to the response of the server to avoid caching
+app.use((req, res, next) => {
+    console.log('Middleware function called');
+    console.log('Request headers:', req.headers);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 // routes
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
@@ -18,14 +30,28 @@ app.post('/api/auth', async(req, res, next)=> {
   }
 });
 
-app.get('/api/auth', async(req, res, next)=> {
-  try {
-    res.send(await User.byToken(req.headers.authorization));
-  }
-  catch(ex){
-    next(ex);
-  }
-});
+app.get('/api/auth', async (req, res, next) => {
+    console.log('Request headers:', req.headers); // log the headers
+
+    console.log('Inside /api/auth route');
+    try {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        throw new Error('Authorization header is missing');
+      }
+  
+      const token = authHeader.split(' ')[1];
+  
+      const user = await User.byToken(token);
+  
+      res.send(user);
+    } catch (err) {
+      const error = new Error(err.message || 'bad credentials');
+      error.status = err.status || 401;
+      next(error);
+    }
+  });
+  
 
 // error handling
 app.use((err, req, res, next) => {
--- db.js
+++ db.js
@@ -1,73 +1,99 @@
-const Sequelize = require('sequelize');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
 const config = {
-  logging: false
+  logging: false,
 };
 
-if(process.env.LOGGING){
+if (process.env.LOGGING) {
   delete config.logging;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db",
+  config
+);
 
-const User = conn.define('user', {
+const User = conn.define("user", {
   username: STRING,
-  password: STRING
+  password: STRING,
 });
 
-User.byToken = async(token)=> {
+User.byToken = async (token) => {
   try {
-    const user = await User.findByPk(token);
-    if(user){
-      return user;
+    const decodedToken = jwt.verify(token, process.env.JWT);
+
+    const user = await User.findByPk(decodedToken.userId);
+
+    if (!user) {
+      throw new Error("User not found");
     }
-    const error = Error('bad credentials');
-    error.status = 401;
-    throw error;
-  }
-  catch(ex){
-    const error = Error('bad credentials');
+
+    return user;
+  } catch (err) {
+    const error = new Error("bad credentials");
     error.status = 401;
     throw error;
   }
 };
 
-User.authenticate = async({ username, password })=> {
-  const user = await User.findOne({
-    where: {
-      username,
-      password
+User.authenticate = async ({ username, password }) => {
+    console.log(`username: ${username}, password: ${password}`);
+    const user = await User.findOne({
+      where: {
+        username,
+        password,
+      },
+    });
+    if (user) {
+      // Create the token payload
+      const payload = {
+        userId: user.id,
+      };
+      // Get the secret key from the environment variables
+      const secretKey = process.env.JWT;
+  
+      // Define the options for the token
+      const options = {
+        expiresIn: "1h", // The token will expire in 1 hour
+      };
+  
+      // Create the token
+      const token = jwt.sign(payload, secretKey, options);
+      console.log(`Token: ${token}`);
+  
+      // return the token
+      return token;
     }
-  });
-  if(user){
-    return user.id; 
-  }
-  const error = Error('bad credentials');
-  error.status = 401;
-  throw error;
-};
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
+  };
+  
 
-const syncAndSeed = async()=> {
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: 'lucy', password: 'lucy_pw'},
-    { username: 'moe', password: 'moe_pw'},
-    { username: 'larry', password: 'larry_pw'}
+    { username: "lucy", password: "lucy_pw" },
+    { username: "moe", password: "moe_pw" },
+    { username: "larry", password: "larry_pw" },
   ];
   const [lucy, moe, larry] = await Promise.all(
-    credentials.map( credential => User.create(credential))
+    credentials.map((credential) => User.create(credential))
   );
   return {
     users: {
       lucy,
       moe,
-      larry
-    }
+      larry,
+    },
   };
 };
 
 module.exports = {
   syncAndSeed,
   models: {
-    User
-  }
+    User,
+  },
 };
